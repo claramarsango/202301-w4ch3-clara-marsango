f5bb828b5734f981422e93f9c62caaa8461c7997
claramarsango <claraxmarsango@gmail.com>
2023-02-08 20:52:26 +0100

    create task creator component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import TaskCreator from "./TaskCreator/TaskCreator";
 
 function App() {
   return (
     <div className="App">
       <header>TodoInput</header>
+      <TaskCreator />
     </div>
   );
 }
