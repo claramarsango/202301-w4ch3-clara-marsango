3f74cdc8ff0a257668737759215555400bb70613
claramarsango <claraxmarsango@gmail.com>
2023-02-08 20:51:11 +0100

    create header tag in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>TodoInput</header>
+    </div>
+  );
 }
 
 export default App;
